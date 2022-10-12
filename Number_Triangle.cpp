8c564fc4841f327fccc0cdcaba81baa6e3c40c02
Milandinesha <115493834+Milandinesha@users.noreply.github.com>
2022-10-12 10:39:51 +0530

    Update Number_Triangle.cpp

--- Number_Triangle.cpp
+++ Number_Triangle.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
+
 using namespace std;
+
 int main() {
-  int i j k l n;
-  cout << "Enter the Range="
-  cot >> n;
-  for (i = 1; i <= a; i+) {
-    for (j = 1; j <= n - i; j+) {
+  
+  int i, j, k, l, n;
+  
+  cout << "Enter the Range=";
+  
+  cin >> n;
+  
+  for (i = 1; i <= n; i++) {
+    
+    for (j = 1; j <= n - i; j++) {
+      
       cout << " ";
     }
-    for (k = 1; k <= i; k-- {
+    
+    for (k = 1; k <= i; k++) {
+      
       cout << k;
+      
     }
-    for (l = i - 1; l >= 1; l++ {
+    
+    for (l = i - 1; l >= 1; l--) {
+      
       cout << l;
+      
     }
+    
     cout << "\n";
+    
   }
+  
   return 0;
+  
 }
